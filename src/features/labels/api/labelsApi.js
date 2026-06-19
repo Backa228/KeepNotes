637a045c32cc5e25ac568e790df5e2b50b8b0a20
Backa228 <andrey.scott01@gmail.com>
2026-06-19 11:01:12 +0300

    lesson19.06
    Оновили таблиці в бд та
    створли функції fetchLabels(), |
    createLabel().

---
+++ src/features/labels/api/labelsApi.js
@@ -0,0 +1,47 @@
+import { supabase } from " .. / .. / .. /shared/api/supabase";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+// READ
+export const fetchLabels = createAsyncThunk(
+    "labels/fetchLabels",
+    async (_, thunkAPI) => {
+        try { 
+            const { data, error } = await supabase
+                .from("labels")
+                .select("*")
+                .order("created_at", { asceding: false })
+            
+            if (error) throw error;
+
+            return data
+
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+)
+
+// READ
+export const createLabels = createAsyncThunk(
+    "labels/createLabels",
+    async ({name, userId}, thunkAPI) => {
+        try { 
+            const { data, error } = await supabase
+                .from("labels")
+                .insert([
+                    {
+                        name,
+                        userId: userId
+                    }
+                ])
+                .select()
+            
+            if (error) throw error;
+
+            return data[0]
+
+        } catch (e) {
+            return thunkAPI.rejectWithValue(e.message);
+        }
+    }
+)
